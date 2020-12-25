e862056d477c49771ff76a005c3a360d791341e4
karthik-prog <59428580+karthik-prog@users.noreply.github.com>
2020-12-25 11:48:29 +0530

    Add files via upload

---
+++ Day18_Animal_Parent_Class.py
@@ -0,0 +1,12 @@
+class  animal:
+    def four_leg(self):
+        print("walking")
+    def mouth(self):
+        print("Eating")
+class lion(animal):
+    def Hunting(self):
+        print("Hunting")
+lA=lion()
+lA.four_leg()
+lA.mouth()
+lA.Hunting()
